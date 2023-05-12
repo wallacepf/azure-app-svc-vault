dfc93deea427f67c9a9e6812f7854ab2a0cc80d8
wallacepf <wallacepf@me.com>
2023-05-12 01:54:24 -0300

    Fixes

--- go.mod
+++ go.mod
@@ -2,18 +2,31 @@ module vault-appsvc
 
 go 1.19
 
-require github.com/hashicorp/vault-client-go v0.3.1
+require (
+	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.6.0
+	github.com/Azure/azure-sdk-for-go/sdk/azidentity v1.3.0
+	github.com/hashicorp/vault-client-go v0.3.1
+)
 
 require (
+	github.com/Azure/azure-sdk-for-go/sdk/internal v1.3.0 // indirect
+	github.com/AzureAD/microsoft-authentication-library-for-go v1.0.0 // indirect
+	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/hashicorp/go-retryablehttp v0.7.2 // indirect
 	github.com/hashicorp/go-rootcerts v1.0.2 // indirect
 	github.com/hashicorp/go-secure-stdlib/strutil v0.1.2 // indirect
+	github.com/kylelemons/godebug v1.1.0 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/pkg/browser v0.0.0-20210911075715-681adbf594b8 // indirect
 	github.com/ryanuber/go-glob v1.0.0 // indirect
+	golang.org/x/crypto v0.9.0 // indirect
 	golang.org/x/exp v0.0.0-20230510235704-dd950f8aeaea // indirect
+	golang.org/x/net v0.10.0 // indirect
 	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
 )
--- main.go
+++ main.go
@@ -6,35 +6,59 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
+	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
 	"github.com/hashicorp/vault-client-go"
 	"github.com/hashicorp/vault-client-go/schema"
 )
 
 func main() {
-	http.HandleFunc("/getsecret", SecretsVault)
+	http.HandleFunc("/getsecret", secretsVault)
 	http.ListenAndServe(":8080", nil)
 }
 
 func initVault() (*vault.Client, context.Context) {
 	ctx := context.Background()
-	client, err := vault.New(
-		vault.WithEnvironment(),
-	); if err != nil {
-		log.Fatal(err)
+
+	cred, err := azidentity.NewDefaultAzureCredential(nil); if err != nil {
+		log.Printf("Error on getting Azure Token: %s", err)
+	}
+	var scopes []string
+	scopes = append(scopes, "https://management.azure.com/")
+
+	tokReqOptions := policy.TokenRequestOptions{}
+	tokReqOptions.Scopes = scopes
+
+	token, err := cred.GetToken(ctx, tokReqOptions); if err != nil {
+		log.Fatalf("Unable to retrieve the token: %s", err)
 	}
 
-	defaultRequest := schema.NewAzureLoginRequestWithDefaults()
+	client, err := vault.New(
+		// vault.WithEnvironment(),
+		vault.WithAddress("http://127.0.0.1:8200"),
+	); if err != nil {
+		log.Fatalf("Cannot connect to the Vault Instance: %s", err)
+	}
+
+	defaultRequest := schema.AzureLoginRequest{
+		Jwt: token.Token,
+		ResourceGroupName: "MyDemoAppRG",
+		Role: "myapp",
+		SubscriptionId: "7f7602dd-85a6-4140-8501-61f2ee9f65a9",
+		ResourceId: "/subscriptions/7f7602dd-85a6-4140-8501-61f2ee9f65a9/resourcegroups/MyDemoAppRG/providers/Microsoft.Web/sites/myapp-demo-pov",
+		
+	}
 	_, err = client.Auth.AzureLogin(
 		ctx,
-		*defaultRequest,
+		defaultRequest,
 	); if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Error when loggin in Vault with Azure Creds: %s", err)
 	}
 
 	return client, ctx
 }
 
-func SecretsVault(w http.ResponseWriter, r *http.Request) {
+func secretsVault(w http.ResponseWriter, r *http.Request) {
 	client, ctx := initVault()
 	s, err := client.Secrets.KvV2Read(ctx, "my-secrets/poc-interbank"); if err != nil {
 		log.Fatalf("Error when reading the secret: %s", err)
